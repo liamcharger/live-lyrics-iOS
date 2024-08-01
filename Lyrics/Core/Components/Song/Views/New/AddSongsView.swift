5c07fe1e35b2e5bf26f9e38c94c0879f38a28664
liamcharger <chargerelectronics19@gmail.com>
2024-08-01 12:18:37 -0400

    AddSongsView: add shared songs to selection

--- Lyrics/Core/Components/Song/Views/New/AddSongsView.swift
+++ Lyrics/Core/Components/Song/Views/New/AddSongsView.swift
@@ -19,16 +19,20 @@ struct AddSongsView: View {
     @State var showSearchBar = false
     @State var isLoading = false
     
-    @State var selectedSongs: [String: Bool] = [:]
+    @State var selectedSongs: [Song] = []
     
     let folder: Folder
     
     var searchableSongs: [Song] {
+        let songs = (mainViewModel.sharedSongs + mainViewModel.songs).sorted(by: { song1, song2 in
+            return song1.title.lowercased() < song2.title.lowercased()
+        })
+        
         if searchText.isEmpty {
-            return mainViewModel.songs
+            return songs
         } else {
             let lowercasedQuery = searchText.lowercased()
-            return mainViewModel.songs.filter ({
+            return songs.filter({
                 $0.title.lowercased().contains(lowercasedQuery)
             })
         }
@@ -36,56 +40,31 @@ struct AddSongsView: View {
     
     func addToFolder() {
         for song in mainViewModel.folderSongs {
-            if let songID = song.id, selectedSongs[songID] == nil {
+            if !selectedSongs.contains(where: { $0.id == song.id }) {
                 mainViewModel.deleteSong(folder, song)
             }
         }
         
-        var songs: [Song] = []
-        let dispatch = DispatchGroup()
-        
-        for(songID, isSelected) in selectedSongs {
-            guard isSelected else {
-                continue
-            }
-            
-            dispatch.enter()
-            self.songViewModel.fetchSong(listen: false, songID) { song in
-                if let song = song {
-                    songs.append(song)
-                }
-                dispatch.leave()
-            } regCompletion: { _ in }
-        }
-        
-        dispatch.notify(queue: .main) {
-            self.songViewModel.moveSongsToFolder(folder: folder, songs: songs) { error in
-                if let error = error {
-                    if error.localizedDescription == "Failed to get document because the client is offline." {
-                        self.errorMessage = "Please connect to the internet to perform this action."
-                        self.showError = true
-                        self.isLoading = false
-                    } else {
-                        self.errorMessage = errorMessage
-                        self.showError = true
-                        self.isLoading = false
-                    }
+        songViewModel.moveSongsToFolder(folder: folder, songs: selectedSongs) { error in
+            if let error = error {
+                if error.localizedDescription == "Failed to get document because the client is offline." {
+                    self.errorMessage = "Please connect to the internet to perform this action."
+                    self.showError = true
                 } else {
-                    presMode.wrappedValue.dismiss()
-                    self.isLoading = false
+                    self.errorMessage = error.localizedDescription
+                    self.showError = true
                 }
+                self.isLoading = false
+            } else {
+                presMode.wrappedValue.dismiss()
+                self.isLoading = false
             }
         }
     }
     
     func checkForSongs() {
-        for song in mainViewModel.folderSongs {
-            if let songID = song.id {
-                let isSongInMainSongs = mainViewModel.songs.contains(where: { $0.id == songID })
-                if isSongInMainSongs {
-                    selectedSongs[songID] = true
-                }
-            }
+        selectedSongs = mainViewModel.folderSongs.filter { folderSong in
+            mainViewModel.songs.contains { $0.id == folderSong.id }
         }
     }
     
@@ -110,7 +89,7 @@ struct AddSongsView: View {
                             .clipShape(Capsule())
                     }
                 }
-                Button(action: {presMode.wrappedValue.dismiss()}) {
+                Button(action: { presMode.wrappedValue.dismiss() }) {
                     Image(systemName: "xmark")
                         .imageScale(.medium)
                         .padding(12)
@@ -145,7 +124,7 @@ struct AddSongsView: View {
             }
             .padding()
             Divider()
-            if mainViewModel.isLoadingSongs || mainViewModel.isLoadingFolderSongs {
+            if mainViewModel.isLoadingSongs || mainViewModel.isLoadingFolderSongs || mainViewModel.isLoadingSharedSongs {
                 VStack {
                     Spacer()
                     ProgressView("Loading")
@@ -180,56 +159,46 @@ struct AddSongsView: View {
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                         }
                         ForEach(searchableSongs) { song in
-                            if song.title == "noSongs" {
-                                Text("No Songs")
-                                    .foregroundColor(Color.gray)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            } else {
-                                Button(action: {
-                                    if let songID = song.id {
-                                        if selectedSongs[songID] != nil {
-                                            selectedSongs[songID] = nil
+                            Button(action: {
+                                if selectedSongs.contains(where: { $0.id == song.id }) {
+                                    selectedSongs.removeAll(where: { $0.id == song.id })
+                                } else {
+                                    selectedSongs.append(song)
+                                }
+                            }) {
+                                HStack(spacing: 7) {
+                                    Text(song.title)
+                                        .lineLimit(1)
+                                    if song.uid != AuthViewModel.shared.currentUser?.id! {
+                                        Image(systemName: "person.2")
+                                            .font(.system(size: 16).weight(.medium))
+                                    }
+                                    Spacer()
+                                    if selectedSongs.contains(where: { $0.id == song.id }) {
+                                        Image(systemName: "checkmark.circle.fill")
+                                            .foregroundColor(.blue)
+                                            .imageScale(.large)
+                                    } else {
+                                        Image(systemName: "circle")
+                                            .imageScale(.large)
+                                    }
+                                }
+                                .padding()
+                                .background(Material.regular)
+                                .foregroundColor(.primary)
+                                .clipShape(Capsule())
+                                .contextMenu {
+                                    Button(action: {
+                                        if selectedSongs.contains(where: { $0.id == song.id }) {
+                                            selectedSongs.removeAll(where: { $0.id == song.id }) // Deselect
                                         } else {
-                                            selectedSongs[songID] = true
+                                            selectedSongs.append(song) // Select
                                         }
-                                    }
-                                }) {
-                                    HStack {
-                                        Text(song.title)
-                                            .lineLimit(1)
-                                        Spacer()
-                                        if let songID = song.id {
-                                            if let isSelected = selectedSongs[songID] {
-                                                Image(systemName: "checkmark.circle.fill")
-                                                    .foregroundColor(.blue)
-                                                    .imageScale(.large)
-                                            } else {
-                                                Image(systemName: "circle")
-                                                    .imageScale(.large)
-                                            }
-                                        }
-                                    }
-                                    .padding()
-                                    .background(Material.regular)
-                                    .foregroundColor(.primary)
-                                    .clipShape(Capsule())
-                                    .contextMenu {
-                                        Button(action: {
-                                            if let songID = song.id {
-                                                if selectedSongs[songID] != nil {
-                                                    selectedSongs[songID] = nil // Deselect
-                                                } else {
-                                                    selectedSongs[songID] = true // Select
-                                                }
-                                            }
-                                        }) {
-                                            if let songID = song.id {
-                                                if selectedSongs[songID] != nil {
-                                                    Label("Deselect", systemImage: "circle")
-                                                } else {
-                                                    Label("Select", systemImage: "checkmark.circle.fill")
-                                                }
-                                            }
+                                    }) {
+                                        if selectedSongs.contains(where: { $0.id == song.id }) {
+                                            Label("Deselect", systemImage: "circle")
+                                        } else {
+                                            Label("Select", systemImage: "checkmark.circle.fill")
                                         }
                                     }
                                 }
@@ -242,6 +211,10 @@ struct AddSongsView: View {
         }
         .onAppear {
             checkForSongs()
+            
+            if mainViewModel.isLoadingSharedSongs {
+                mainViewModel.fetchSharedSongs()
+            }
         }
         .onChange(of: mainViewModel.folderSongs) { _ in
             checkForSongs()
