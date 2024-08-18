74fc63c4492d1022fa519fef338de92a209ca880
liamcharger <chargerelectronics19@gmail.com>
2024-08-18 11:20:07 -0400

    UI: optimize HeaderView for iPad

--- Lyrics/Core/Components/HeaderView.swift
+++ Lyrics/Core/Components/HeaderView.swift
@@ -11,8 +11,10 @@ struct HeaderView: View {
     let title: String
     let icon: String
     let color: Color
-    var geo: GeometryProxy
+    let geo: GeometryProxy
     let counter: String
+    
+    let idiom: UIUserInterfaceIdiom = UIDevice.current.userInterfaceIdiom
     
     init(_ title: String, icon: String, color: Color, geo: GeometryProxy, counter: String? = nil) {
         self.title = title
@@ -22,20 +24,28 @@ struct HeaderView: View {
         self.counter = counter ?? ""
     }
     
+    func width(for shape: Int) -> CGFloat {
+        if shape == 0 {
+            return idiom == .phone ? geo.size.width / 4 : geo.size.width / 6.5
+        } else {
+            return idiom == .phone ? geo.size.width / 5 : geo.size.width / 8
+        }
+    }
+    
     var body: some View {
         ZStack {
             Group {
                 Circle()
                     .foregroundColor(color)
-                    .frame(width: geo.size.width / 4, height: geo.size.width / 4)
+                    .frame(width: width(for: 0), height: width(for: 0))
                     .offset(x: -100, y: -30)
                 Circle()
                     .foregroundColor(color)
-                    .frame(width: geo.size.width / 5, height: geo.size.width / 5)
+                    .frame(width: width(for: 1), height: width(for: 1))
                     .offset(x: 10, y: 15)
             }
             .offset(x: 65, y: 35)
-            .blur(radius: 58)
+            .blur(radius: idiom == .phone ? 58: 116)
             VStack(alignment: .leading, spacing: 14) {
                 FAText(iconName: icon, size: 45)
                 VStack(alignment: .leading, spacing: 7) {
@@ -51,5 +61,12 @@ struct HeaderView: View {
             .padding(.vertical, 25)
             .padding(.horizontal, 10)
         }
+    }
+}
+
+#Preview {
+    GeometryReader { geo in
+        HeaderView("Recently Deleted", icon: "trash-can", color: .red, geo: geo)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
